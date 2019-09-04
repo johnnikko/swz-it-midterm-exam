4be5595fe5260cf56d86c1587c90b8a1cc06d5b6
johnnikko <nikko06albarado@gmail.com>
2019-09-04 18:58:20 +0800

    exam edited and add admin

--- app/models/disaster.rb
+++ app/models/disaster.rb
@@ -1,9 +1,19 @@
 class Disaster < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+
   has_many :comments, dependent: :destroy
+  before_destroy :ensure_deletable, prepend: true
 
   validates :title, presence: true, length: {minimun:3,maximum:50}
   validates :description, presence: true
+
+  def ensure_deletable
+    raise "You only delete your own information" if users_disaster?
+  end
+
+  def users_disaster?
+    self.user_id == current_user.id
+  end
 
 end
